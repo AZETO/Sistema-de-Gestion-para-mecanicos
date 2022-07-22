ab77a8b5ba18e79a754409b6eec94a8a33e86c8c
AZETO <104459298+AZETO@users.noreply.github.com>
2022-07-22 01:04:16 -0400

    Add files via upload

---
+++ Models (ACTUAL CLASES DEL PROYECTO YA INTEGRADO)/perfil_taller.cs
@@ -0,0 +1,19 @@
+﻿namespace PaginaTallerMeca.Models
+{
+    public class perfil_taller
+    {
+        public int ID_perfil{ get; set; }
+
+        public string nombre_mecanico { get; set; }
+        public string nombre_taller { get; set; }
+        public string direccion_taller { get; set; }
+        public string venta_producto { get; set; }
+        public string servicio { get; set; }
+        public string telefono_taller { get; set; }
+
+
+
+
+
+    }
+}
