010096a7379493e919039d5598a5467b2f9ea11d
AZETO <104459298+AZETO@users.noreply.github.com>
2022-07-22 01:08:12 -0400

    Add files via upload

---
+++ Molde (ANTIGUAS CLASES PEDIDAS AL PRINCIPIO, NO INTEGRAS AL PROYECTO ACTUAL)/Catalogoproducto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoTalleresMecanicos.Molde
+{
+    internal class Catalogoproducto
+    {
+        private int catalogoproducto_id;
+        private String catalogoproducto_nombre;
+        private String catalogoproducto_precio;
+        private String catalogoproducto_stock;
+
+        public int Catalogoproducto_id { get => catalogoproducto_id; set => catalogoproducto_id = value; }
+        public string Catalogoproducto_nombre { get => catalogoproducto_nombre; set => catalogoproducto_nombre = value; }
+        public string Catalogoproducto_precio { get => catalogoproducto_precio; set => catalogoproducto_precio = value; }
+        public string Catalogoproducto_stock { get => catalogoproducto_stock; set => catalogoproducto_stock = value; }
+
+        public Catalogoproducto()
+        {
+        }
+
+        public Catalogoproducto(int catalogoproducto_id, string catalogoproducto_nombre, string catalogoproducto_precio, string catalogoproducto_stock)
+        {
+            Catalogoproducto_id = catalogoproducto_id;
+            Catalogoproducto_nombre = catalogoproducto_nombre;
+            Catalogoproducto_precio = catalogoproducto_precio;
+            Catalogoproducto_stock = catalogoproducto_stock;
+         
+        }
+    }
+}
---
+++ Molde (ANTIGUAS CLASES PEDIDAS AL PRINCIPIO, NO INTEGRAS AL PROYECTO ACTUAL)/Moduloboucher.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoTalleresMecanicos.Molde
+{
+    internal class Moduloboucher
+    {
+        private int moduloboucher_id;
+        private String moduloboucher_nombrecliente;
+        private String moduloboucher_correocliente;
+        private String moduloboucher_rutcliente;
+        private String moduloboucher_nombremecanico;
+        private String moduloboucher_tallermecanico;
+        private String moduloboucher_direcciontaller;
+        private String moduloboucher_correomecanico;
+        private Int32  moduloboucher_precio;
+        private DateTime moduloboucher_fecha;
+
+        public int Moduloboucher_id { get => moduloboucher_id; set => moduloboucher_id = value; }
+        public string Moduloboucher_nombrecliente { get => moduloboucher_nombrecliente; set => moduloboucher_nombrecliente = value; }
+        public string Moduloboucher_correocliente { get => moduloboucher_correocliente; set => moduloboucher_correocliente = value; }
+        public string Moduloboucher_rutcliente { get => moduloboucher_rutcliente; set => moduloboucher_rutcliente = value; }
+        public string Moduloboucher_nombremecanico { get => moduloboucher_nombremecanico; set => moduloboucher_nombremecanico = value; }
+        public string Moduloboucher_tallermecanico { get => moduloboucher_tallermecanico; set => moduloboucher_tallermecanico = value; }
+        public string Moduloboucher_direcciontaller { get => moduloboucher_direcciontaller; set => moduloboucher_direcciontaller = value; }
+        public string Moduloboucher_correomecanico { get => moduloboucher_correomecanico; set => moduloboucher_correomecanico = value; }
+        public int Moduloboucher_precio { get => moduloboucher_precio; set => moduloboucher_precio = value; }
+        public DateTime Moduloboucher_fecha { get => moduloboucher_fecha; set => moduloboucher_fecha = value; }
+
+        public Moduloboucher()
+        {
+        }
+
+        public Moduloboucher(int moduloboucher_id, string moduloboucher_nombrecliente, string moduloboucher_correocliente, string moduloboucher_rutcliente, string moduloboucher_nombremecanico, string moduloboucher_tallermecanico, string moduloboucher_direcciontaller, string moduloboucher_correomecanico, int moduloboucher_precio, DateTime moduloboucher_fecha)
+        {
+            Moduloboucher_id = moduloboucher_id;
+            Moduloboucher_nombrecliente = moduloboucher_nombrecliente;
+            Moduloboucher_correocliente = moduloboucher_correocliente;
+            Moduloboucher_rutcliente = moduloboucher_rutcliente;
+            Moduloboucher_nombremecanico = moduloboucher_nombremecanico;
+            Moduloboucher_tallermecanico = moduloboucher_tallermecanico;
+            Moduloboucher_direcciontaller = moduloboucher_direcciontaller;
+            Moduloboucher_correomecanico = moduloboucher_correomecanico;
+            Moduloboucher_precio = moduloboucher_precio;
+            Moduloboucher_fecha = moduloboucher_fecha;
+          
+        }
+    }
+}
