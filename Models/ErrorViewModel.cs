8ff281fed54a406284471f09af2c70ad2c8ee50f
AZETO <104459298+AZETO@users.noreply.github.com>
2022-07-22 01:02:20 -0400

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace PaginaTallerMeca.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
