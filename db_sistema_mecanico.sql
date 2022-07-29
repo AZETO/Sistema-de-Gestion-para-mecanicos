cd61cfbfd093dbd7c49b35b547e507dd5d1ad122
AZETO <104459298+AZETO@users.noreply.github.com>
2022-07-29 13:30:53 -0400

    Add files via upload

---
+++ db_sistema_mecanico.sql
@@ -0,0 +1,225 @@
+-- MySQL dump 10.13  Distrib 8.0.29, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: db_sistema_mecanico
+-- ------------------------------------------------------
+-- Server version	8.0.29
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `boleta_canje`
+--
+
+DROP TABLE IF EXISTS `boleta_canje`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `boleta_canje` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre_cliente` varchar(100) DEFAULT NULL,
+  `correo_cliente` varchar(100) DEFAULT NULL,
+  `rut_cliente` varchar(100) DEFAULT NULL,
+  `nombre_taller` varchar(100) DEFAULT NULL,
+  `direccion_taller` varchar(100) DEFAULT NULL,
+  `correo_mecanico` varchar(100) DEFAULT NULL,
+  `servicio_venta` varchar(300) DEFAULT NULL,
+  `fecha` int DEFAULT NULL,
+  `precio` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `boleta_canje`
+--
+
+LOCK TABLES `boleta_canje` WRITE;
+/*!40000 ALTER TABLE `boleta_canje` DISABLE KEYS */;
+/*!40000 ALTER TABLE `boleta_canje` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `inventario`
+--
+
+DROP TABLE IF EXISTS `inventario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `inventario` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `nombre_producto` varchar(100) DEFAULT NULL,
+  `imagen` varchar(200) DEFAULT NULL,
+  `descripcion` varchar(300) DEFAULT NULL,
+  `stock` int DEFAULT NULL,
+  `precio` int DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `inventario`
+--
+
+LOCK TABLES `inventario` WRITE;
+/*!40000 ALTER TABLE `inventario` DISABLE KEYS */;
+INSERT INTO `inventario` VALUES (1,'Cubierta de volante',NULL,'Nuevo',2,5000),(2,'Juegos de llaves',NULL,'Nuevo',6,18000),(3,'Gatos hidráulicos',NULL,'Nuevo',5,34000),(4,'Compresor de aire',NULL,'Nuevo',11,46000),(5,'Multímetro automotriz',NULL,'Nuevo',4,40000);
+/*!40000 ALTER TABLE `inventario` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `registro_auto`
+--
+
+DROP TABLE IF EXISTS `registro_auto`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `registro_auto` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `ID_dueño` int DEFAULT NULL,
+  `marca` varchar(50) DEFAULT NULL,
+  `modelo` varchar(100) DEFAULT NULL,
+  `año` varchar(2100) DEFAULT NULL,
+  `tipo_motor` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `registro_auto`
+--
+
+LOCK TABLES `registro_auto` WRITE;
+/*!40000 ALTER TABLE `registro_auto` DISABLE KEYS */;
+/*!40000 ALTER TABLE `registro_auto` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `servicio`
+--
+
+DROP TABLE IF EXISTS `servicio`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `servicio` (
+  `id_servicio` int NOT NULL,
+  `nombre` varchar(25) DEFAULT NULL,
+  `descripcion` varchar(300) DEFAULT NULL,
+  `fecha` date DEFAULT NULL,
+  PRIMARY KEY (`id_servicio`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `servicio`
+--
+
+LOCK TABLES `servicio` WRITE;
+/*!40000 ALTER TABLE `servicio` DISABLE KEYS */;
+/*!40000 ALTER TABLE `servicio` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuario_cliente`
+--
+
+DROP TABLE IF EXISTS `usuario_cliente`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuario_cliente` (
+  `id_cliente` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(200) DEFAULT NULL,
+  `usuario` varchar(200) DEFAULT NULL,
+  `rut` varchar(25) DEFAULT NULL,
+  `telefono` varchar(200) DEFAULT NULL,
+  `direccion` varchar(100) DEFAULT NULL,
+  `correo` varchar(100) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  `fecha` date DEFAULT NULL,
+  PRIMARY KEY (`id_cliente`)
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuario_cliente`
+--
+
+LOCK TABLES `usuario_cliente` WRITE;
+/*!40000 ALTER TABLE `usuario_cliente` DISABLE KEYS */;
+INSERT INTO `usuario_cliente` VALUES (29,'Pedro Pedrales','Elpepe','19493693-1','923517596','Mikasa 117','ejemplo@gmail.com','12345',NULL),(30,'Geepeto Marmol','pin8','19463259-1','123456799','La otra casa 123','ejemplo2@gmail.com','54321',NULL),(31,'','','','','','','',NULL);
+/*!40000 ALTER TABLE `usuario_cliente` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuario_mecanico`
+--
+
+DROP TABLE IF EXISTS `usuario_mecanico`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuario_mecanico` (
+  `ID_mecanico` int NOT NULL AUTO_INCREMENT,
+  `usuario` varchar(100) DEFAULT NULL,
+  `nombre` varchar(200) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `password` varchar(200) DEFAULT NULL,
+  `nombre_taller` varchar(100) DEFAULT NULL,
+  `direccion_taller` varchar(100) DEFAULT NULL,
+  `venta_producto` varchar(100) DEFAULT NULL,
+  `telefono_taller` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`ID_mecanico`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuario_mecanico`
+--
+
+LOCK TABLES `usuario_mecanico` WRITE;
+/*!40000 ALTER TABLE `usuario_mecanico` DISABLE KEYS */;
+INSERT INTO `usuario_mecanico` VALUES (4,'ubunto','Ububue','osas@gmail.com','qwerty','El Raspon','Al lado de mikasa 5584','si','19195168');
+/*!40000 ALTER TABLE `usuario_mecanico` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `valoracion`
+--
+
+DROP TABLE IF EXISTS `valoracion`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `valoracion` (
+  `ID_valo` int NOT NULL AUTO_INCREMENT,
+  `tipo_servicio` varchar(100) DEFAULT NULL,
+  `comentario` varchar(100) DEFAULT NULL,
+  `nota` int DEFAULT NULL,
+  PRIMARY KEY (`ID_valo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `valoracion`
+--
+
+LOCK TABLES `valoracion` WRITE;
+/*!40000 ALTER TABLE `valoracion` DISABLE KEYS */;
+/*!40000 ALTER TABLE `valoracion` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-07-29 13:29:07
